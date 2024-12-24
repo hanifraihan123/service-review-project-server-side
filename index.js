294fe91e4b2a0dbf5b959c81b376d64bf21de609
hanifraihan123 <hanifraihan561@gmail.com>
2024-12-25 00:50:24 +0600

    review post & get API created

--- index.js
+++ index.js
@@ -2,12 +2,32 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173',],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
+const verifyToken = (req,res,next) => {
+  const token = req.cookies?.token;
+  if(!token){
+    return res.status(401).send({message: 'Unauthorized Access'})
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'Unauthorized Access'})
+    }
+    req.user = decoded;
+    next();
+  })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.neb49.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -38,6 +58,28 @@ async function run() {
       res.send(result);
     })
 
+    // jwt token related APIs
+    app.post('/jwt', (req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '10h'});
+        res
+        .cookie('token',token,{
+            httpOnly: true,
+            secure: false
+            })
+        .send({success: true})
+    })
+
+    app.post('/logout', (req,res)=>{
+      res
+      .clearCookie('token', {
+        httpOnly: true,
+        secure: false
+      })
+      .send({success: true})
+    })
+
     // Read all services
     app.get('/services', async(req,res)=>{
       const cursor = serviceCollection.find();
@@ -61,11 +103,22 @@ async function run() {
     })
 
     // Get a single service by email
-    app.get('/services/:email', async(req,res)=>{
+    app.get('/services/:email',verifyToken, async(req,res)=>{
       const email = req.params.email;
       const query = {email}
+      if(req.user.email !== req.params.email){
+        return res.status(403).send({message: 'Forbidden Access'})
+      }
       const result = await serviceCollection.find(query).toArray();
       res.send(result);
+    })
+
+    // Service Delete related APIs
+    app.delete('/service/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await serviceCollection.deleteOne(query)
+      res.send(result)
     })
 
     // Review related APIs
@@ -80,6 +133,14 @@ async function run() {
       const cursor = reviewCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    })
+
+    // Get specific Review
+    app.get('/review', async(req,res)=>{
+      const id = req.query.id;
+      const query = {serviceId: id}
+      const result = await reviewCollection.find(query).toArray();
+      res.send(result)
     })
 
   } finally {
